4aeca582ba97d979a40057bf0219b8f75ec1ef00
thaisBertoldi <thais_brt@hotmail.com>
2023-11-02 18:32:44 -0300

    feat: metodo delete

--- src/main/java/br/thais/resources/UserResource.java
+++ src/main/java/br/thais/resources/UserResource.java
@@ -17,11 +17,15 @@ import java.util.stream.Collectors;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    public static final String ID = "/{id}";
+
     @Autowired
     private ModelMapper mapper;
 
     @Autowired
     private UserService service;
+
+
 
     @GetMapping(value = "/id")
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
@@ -37,14 +41,19 @@ public class UserResource {
     @PostMapping
     public ResponseEntity<UserDTO> create(@RequestBody UserDTO obj) {
         URI uri = ServletUriComponentsBuilder
-            .fromCurrentRequest().path("/{id}").buildAndExpand(service.create(obj).getId()).toUri();
+            .fromCurrentRequest().path(ID).buildAndExpand(service.create(obj).getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
 
-    @PutMapping(value = "/{id}")
+    @PutMapping(value = ID)
     public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO obj) {
         obj.setId(id);
         return ResponseEntity.ok().body(mapper.map(service.update(obj), UserDTO.class));
     }
 
+    @DeleteMapping(value = ID)
+    public ResponseEntity<UserDTO> delete(@PathVariable Integer id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
