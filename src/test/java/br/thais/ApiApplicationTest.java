d875d10251feaaa5c63380af05735b9e94130690
thaisBertoldi <thais_brt@hotmail.com>
2023-11-03 19:57:55 -0300

    test: teste para classe principal

---
+++ src/test/java/br/thais/ApiApplicationTest.java
@@ -0,0 +1,13 @@
+package br.thais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTest {
+
+    @Test
+    void main() {
+        ApiApplication.main(new String[] {});
+    }
+}
