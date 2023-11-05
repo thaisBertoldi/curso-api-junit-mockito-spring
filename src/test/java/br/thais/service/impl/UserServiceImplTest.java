3308d362a6e59bd157558b6193d6cb49f0a40096
thaisBertoldi <thais_brt@hotmail.com>
2023-11-04 21:10:14 -0300

    test: criado teste para o service

---
+++ src/test/java/br/thais/service/impl/UserServiceImplTest.java
@@ -0,0 +1,53 @@
+package br.thais.service.impl;
+
+import br.thais.domain.User;
+import br.thais.domain.dto.UserDTO;
+import br.thais.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserServiceImplTest {
+
+    public static final int ID = 1;
+    public static final String NAME = "Teste";
+    public static final String EMAIL = "teste@email";
+    public static final String PASSWORD = "123";
+    @InjectMocks
+    private UserServiceImpl service;
+
+    @Mock
+    private UserRepository repository;
+
+    @Mock
+    private ModelMapper mapper;
+
+    private User user;
+    private UserDTO userDTO;
+    private Optional<User> optionalUser;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void findById() {
+    }
+
+    private void startUser() {
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+        optionalUser = Optional.of(new User(ID, NAME, EMAIL, PASSWORD));
+    }
+}
