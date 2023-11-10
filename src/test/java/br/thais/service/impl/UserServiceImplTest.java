043611cfa659f201e36b27f9303dede032bbbadf
thaisBertoldi <thais_brt@hotmail.com>
2023-11-10 19:45:08 -0300

    test: correcao teste excecao

--- src/test/java/br/thais/service/impl/UserServiceImplTest.java
+++ src/test/java/br/thais/service/impl/UserServiceImplTest.java
@@ -103,9 +103,11 @@ class UserServiceImplTest {
         when(repository.findByEmail(anyString())).thenReturn(optionalUser);
 
         try {
+            optionalUser.get().setId(2);
             service.create(userDTO);
         } catch (Exception ex) {
             assertEquals(DataIntegratyViolationException.class, ex.getClass());
+            assertEquals("E-mail já cadastrado no sistema", ex.getMessage());
         }
     }
 
