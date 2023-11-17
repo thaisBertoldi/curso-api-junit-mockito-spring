fc54cb5cb84e4679f4a7307da7b9ea92c0bb1db5
thaisBertoldi <thais_brt@hotmail.com>
2023-11-17 19:25:25 -0300

    test: teste findById do resource

--- src/test/java/br/thais/resources/UserResourceTest.java
+++ src/test/java/br/thais/resources/UserResourceTest.java
@@ -4,11 +4,20 @@ import br.thais.domain.User;
 import br.thais.domain.dto.UserDTO;
 import br.thais.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UserResourceTest {
@@ -35,6 +44,29 @@ class UserResourceTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
         startUser();
+    }
+
+    @Test
+    void whenFindByIdWithSuccess() {
+        when(service.findById(anyInt())).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resource.findById(ID);
+
+        assertThat(ResponseEntity.class)
+                .isNotNull()
+                .isEqualTo(response.getClass());
+        assertThat(UserDTO.class)
+                .isNotNull()
+                .isEqualTo(response.getBody().getClass());
+        assertThat(response.getBody().getId())
+                .isEqualTo(ID);
+        assertThat(response.getBody().getName())
+                .isEqualTo(NAME);
+        assertThat(response.getBody().getEmail())
+                .isEqualTo(EMAIL);
+        assertThat(response.getBody().getPassword())
+                .isEqualTo(PASSWORD);
     }
 
     private void startUser() {
