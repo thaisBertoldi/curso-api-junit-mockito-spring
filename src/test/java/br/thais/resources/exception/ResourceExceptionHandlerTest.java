278dce807fe105863dec2894370fe7855e9950bf
thaisBertoldi <thais_brt@hotmail.com>
2023-11-23 20:57:43 -0300

    test: teste para exceçao de objeto nao encontrado

--- src/test/java/br/thais/resources/exception/ResourceExceptionHandlerTest.java
+++ src/test/java/br/thais/resources/exception/ResourceExceptionHandlerTest.java
@@ -1,9 +1,17 @@
 package br.thais.resources.exception;
 
+import br.thais.service.exception.ObjectionNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ResourceExceptionHandlerTest {
@@ -14,6 +22,26 @@ class ResourceExceptionHandlerTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testObjectionNotFoundException() {
+        ResponseEntity<StandardError> response = exceptionHandler.objectNotFound(
+                new ObjectionNotFoundException("Objeto não encontrado"),
+                new MockHttpServletRequest()
+        );
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertThat(response.getStatusCode())
+                .isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getClass())
+                .isEqualTo(ResponseEntity.class);
+        assertThat(response.getBody().getClass())
+                .isEqualTo(StandardError.class);
+        assertThat(response.getBody().getError())
+                .isEqualTo("Objeto não encontrado");
+        assertThat(response.getBody().getStatus())
+                .isEqualTo(404);
     }
 
 }
